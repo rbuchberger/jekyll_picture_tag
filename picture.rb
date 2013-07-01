bbc43df9a3cf7d5ac841b3bfbc969f3b0b6b7534
Rob Wierzbowski <hello@robwierzbowski.com>
2013-07-01 16:48:21 -0400

    Fix instance variable assignment, source reference problems. Minimagick resizing and cropping working. Hacking away.

--- picture.rb
+++ picture.rb
@@ -9,7 +9,9 @@
 
 ### ruby 1.9+
 
+require 'fileutils'
 require 'mini_magick'
+require 'digest/md5'
 
 module Jekyll
 
@@ -17,55 +19,76 @@ module Jekyll
 
     def initialize(tag_name, markup, tokens)
 
-      if @tag = /^(?:(?<preset>[^\s.:]+)\s+)?(?<image_src>[^\s]+\.[a-zA-Z0-9]{3,4})\s*(?<sources_src>(?:(source_[^\s:]+:\s+[^\s]+\.[a-zA-Z0-9]{3,4})\s*)+)?(?<html_attr>[\s\S]+)?$/.match(markup)
-        @preset = @tag[:preset] || 'default'
-        @image_src = @tag[:image_src]
 
-        @sources = {}
-        @tag[:sources_src] || ''.split.each_slice(2) do |set|
-          @sources.merge! Hash[*set]
+      if tag = /^(?:(?<preset>[^\s.:]+)\s+)?(?<image_src>[^\s]+\.[a-zA-Z0-9]{3,4})\s*(?<source_src>(?:(source_[^\s:]+:\s+[^\s]+\.[a-zA-Z0-9]{3,4})\s*)+)?(?<html_attr>[\s\S]+)?$/.match(markup)
+
+        @preset = tag[:preset] || 'default'
+        @image_src = tag[:image_src]
+        @source_src = {}
+        if tag[:source_src]
+          @source_src = Hash[ *tag[:source_src].gsub(/:/, '').split ]
         end
-
         @html_attr = {}
-        @tag[:html_attr] || ''.scan(/(?<attr>[^\s="]+)(?:="(?<value>[^"]+)")?\s?/).each do |html_attr|
-          @html_attr.merge! Hash[*html_attr]
+        if tag[:html_attr]
+          tag[:html_attr].scan(/(?<attr>[^\s="]+)(?:="(?<value>[^"]+)")?\s?/).each do |html_attr|
+            @html_attr.merge! Hash[*html_attr]
+          end
         end
+
       else
         raise SyntaxError.new("Your picture tag doesn't seem to be formatted correctly. Try {% picture [preset_name] path/to/img.jpg [source_key: path/to/alt/img.jpg] [attribute=\"value\"] %}.")
       end
       super
     end
 
+    ### Ruby style assignment notes
+    ### If parent_id is nil or undefined before that line you can simply write:
+    ### parent_id = params[:parent_id] unless params[:parent_type] == "Order"
+
+    ### parent_id = (params[:parent_type] == "Order") ? nil : params[:parent_id]
+    ### Alternatively:
+
+    ### parent_id = if (params[:parent_type] == "Order")
+    ###     nil
+    ### else
+    ###     params[:parent_id]
+    ### end
+
+    ### @going, @not_going = invite.accepted ? ['selected', ''] : ['', 'selected']
+    ### w, x = y, z is the same as w, x = [y, z], so this works just fine and there is no repetition.
+
+    ##### I like this!
+    ### if cond; foo else bar end
+    ### if cond then foo else bar end
+
+
     def render(context)
 
+      ### All vars are REFERENCES
+      ### DUDE ^^^^^^^^^^^^^^^^^^
+
+      # Gather settings
       site = context.registers[:site]
       settings = site.config['picture']
-
       site_path = site.source
-      asset_path = settings['asset_path'] || ''
-      generated_path = settings['generated_path'] || File.join(asset_path, 'generated')
-
       markup = settings['markup'] || 'picturefill'
+      asset_path = settings['asset_path'] || ''
+      gen_path = settings['generated_path'] || File.join(asset_path, 'generated')
 
-      sources = settings['presets'][@preset]
-
-      puts sources.class
-      puts "-----------------"
-
-      html_attr = sources.delete('attr')
-      ppi = sources['ppi'] ? sources.delete('ppi').sort.reverse : nil
+      # Create sources object and settings
+      # Deep copy preset for manipulation. Is there a better way?
+      sources = Marshal.load(Marshal.dump(settings['presets'][@preset]))
+      html_attr = if sources['attr'] then sources.delete('attr').merge!(@html_attr) else @html_attr end
+      ppi = if sources['ppi'] then sources.delete('ppi').sort.reverse else nil end
       source_keys = sources.keys
 
-      # Process html_attr
-      html_attr.merge!(@html_attr)
-
+      # Process attributes
       if markup == 'picturefill'
         html_attr['data-picture'] = nil
-        html_attr['data-alt'] = @html_attr.delete('alt')
+        html_attr['data-alt'] = html_attr.delete('alt')
       end
 
-      # Process html_attr into string.
-      html_attr_string = ""
+      html_attr_string = ''
       html_attr.each {|key, value|
         if value
           html_attr_string += "#{key}=\"#{value}\" "
@@ -74,15 +97,20 @@ module Jekyll
         end
       }
 
-      # Process source
+      # Process sources
+
+      ### check if sources don't exist in preset, raise error
+      if (@source_src.keys - source_keys).any?
+        raise SyntaxError.new("You're trying to specify an image for a source that doesn't exist. Please check picture: presets: #{@preset} in your _config.yml for the list of available sources.")
+      end
 
       # Add image path for each source
       sources.each { |key, value|
-        sources[key][:src] = @sources.fetch(key, @image_src)
+        sources[key][:src] = @source_src[key] || @image_src
       }
 
-      ### check if sources don't exist in preset, raise error?
-      ### raise SyntaxError.new("#{@sources_src[key]} doesn't exist in #{@preset}. Please check _config.yml for available sources.")
+
+      # Process PPIs
 
       # Add resolution based sources
 
@@ -111,93 +139,97 @@ module Jekyll
 
       # Generate sized images
       sources.each { |key, source|
-        sources[source][:generated_src] = generate_image(source, site_path, asset_path, generated_path)
+        sources[key][:generated_src] = generate_image(source, site_path, asset_path, gen_path)
       }
 
       # Construct and return tag
 
-      if settings['markup'] == 'picturefill'
+      "<pre>Hey dog!</pre>"
 
-      ### Not updated for new variables
-      # <span @tag[:attributes]>
-      #   each source_key in @tag[:sources]
-      #     if !source_key['source'], source_key['source'] = @image_src
-      #   <span data-src="source_key[img_path]" (if media) data-media="source_key[media]"></span>
-      #   endeach
-      #
-      #   <noscript>
-      #     <img src="@tag[:sources][source_default][img_path]" alt="@tag[:alt]">
-      #   </noscript>
-      # </span>
+      # if settings['markup'] == 'picturefill'
 
-      elsif settings['markup'] == 'picture'
+      # ### Not updated for new variables
+      # # <span tag[:attributes]>
+      # #   each source_key in @tag[:sources]
+      # #     if !source_key['source'], source_key['source'] = @image_src
+      # #   <span data-src="source_key[img_path]" (if media) data-media="source_key[media]"></span>
+      # #   endeach
+      # #
+      # #   <noscript>
+      # #     <img src="@tag[:sources][source_default][img_path]" alt="@tag[:alt]">
+      # #   </noscript>
+      # # </span>
 
-      ### Not updated for new variables
-      # <picture @tag[:attributes]>
-      #   each source_key in @tag[:sources]
-      #   <source src="source_key[img_path]"
-      #           (if media) media="source_key[media]">
-      #    <img src="small.jpg" alt="">
-      #    <p>@tag[:alt]</p>
-      # </picture>
+      # elsif settings['markup'] == 'picture'
 
-      end
+      # ### Not updated for new variables
+      # # <picture @tag[:attributes]>
+      # #   each source_key in @tag[:sources]
+      # #   <source src="source_key[img_path]"
+      # #           (if media) media="source_key[media]">
+      # #    <img src="small.jpg" alt="">
+      # #    <p>@tag[:alt]</p>
+      # # </picture>
+
+      # end
     end
 
     def generate_image(source, site_path, asset_path, gen_path)
 
-      puts source.class
-
-      puts '------------'
-
-      ### Source input
-      # source_default:
-      #   width: "500"
-      #   height: "200"
-      #   :src: somepath.com/this.img
+      ### We should hash the images to, for cache busting.
 
       if source['width'].nil? && source['height'].nil?
-        raise SyntaxError.new("Source keys must have at least one of width and height. Please check _config.yml.")
+        raise SyntaxError.new("Source keys must have at least one of width and height in the _config.yml.")
       end
 
-      ### Damn, this is so many vars. Can it be simplified?
-
-      ori_dir = File.dirname(source[:src])
+      src_dir = File.dirname(source[:src])
       ext = File.extname(source[:src])
-      ori_name = File.basename(source[:src], ext)
-      ori_image = MiniMagick::Image.open(File.join(site_path, asset_path, source[:src]))
+      src_name = File.basename(source[:src], ext)
 
-      ori_ratio = ori_image[:width]/ori_image[:height]
+      # file open
+      # hash
+      # add to minim read
+
+      src_image = MiniMagick::Image.open(File.join(site_path, asset_path, source[:src]))
+      src_ratio = src_image[:width].to_f / src_image[:height].to_f
+
+      #### Hash orig file
+      #### grab 4 digets
+      #### add to dist file name to check
+
+      # digest = Digest::MD5.hexdigest()
+      # puts digest
+
 
       ### Need to round calcs, or does minimaj take care of that?
-      gen_width = source['width'].to_i || ori_ratio * source['height'].to_i
-      gen_height = source['height'].to_i || ori_ratio/source['width'].to_i
+      ### Clean up all the to_i/f in here
 
-      gen_name = "#{ori_name}-#{gen_width}-#{gen_height}"
-      gen_absolute_path = File.join(site_path, gen_path, ori_dir, gen_name + ext)
-      gen_return_path = File.join(gen_path, ori_dir, gen_name + ext)
+      gen_width = source['width'] ? source['width'].to_i : (src_ratio * source['height'].to_f).to_i
+      gen_height = source['height'] ? source['height'].to_i : (source['width'].to_f / src_ratio).to_i
+      gen_ratio = gen_width.to_f/gen_height.to_f
+
+      gen_name = "#{src_name}-#{gen_width}-#{gen_height}"
+      gen_absolute_path = File.join(site_path, gen_path, src_dir, gen_name + ext)
+      gen_return_path = File.join(gen_path, src_dir, gen_name + ext)
 
       if not File.exists?(gen_absolute_path)
-        ori_image.combine_options do |i|
-          i.resize "#{gen_width}-#{gen_height}^"
 
-          # get which doesn't match?
-          if i[:width] != gen_width
-            i.shave "#{(i[:width] - gen_width)/2}x0"
-          elsif i[:height] != gen_height
-            i.shave "0x#{(i[:height] - gen_height)/2}"
-          end
+        ### create the directory if it doesnt exist
+        FileUtils.mkdir_p(File.join(site_path, gen_path)) unless File.exist?(File.join(site_path, gen_path))
 
-          ### Do we need an absolute path, or can we do everything locally?
-          ### What is cwd?
-          i.write gen_absolute_path
+        src_image.combine_options do |i|
+          i.resize "#{gen_width}x#{gen_height}^"
+          i.gravity "center"
+          i.crop "#{gen_width}x#{gen_height}+0+0"
         end
+
+        src_image.write gen_absolute_path
       end
 
       gen_return_path
     end
+
   end
 end
 
 Liquid::Template.register_tag('picture', Jekyll::Picture)
-picture = Jekyll::Picture.new("","preset_name path/to/img.jpg source_anything: path/to/alt/img.jpg source_second: path/to/second/img.jpg alt=\"alt_text\" disabled title=\"title text\"", "")
