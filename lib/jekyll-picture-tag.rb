19c337611abbe56d6abe027ba07d92837c5db664
Admin@airvox.it <jonathan@airvox.it>
2016-06-28 09:59:16 +0100

    Allow spaces in filename, requires quotes

--- lib/jekyll-picture-tag.rb
+++ lib/jekyll-picture-tag.rb
@@ -21,6 +21,7 @@ require 'pathname'
 require 'digest/md5'
 require 'mini_magick'
 require 'fastimage'
+require 'uri'
 
 module Jekyll
 
@@ -40,7 +41,7 @@ module Jekyll
       site = context.registers[:site]
       settings = site.config['picture']
       url = site.config['url']
-      markup = /^(?:(?<preset>[^\s.:\/]+)\s+)?(?<image_src>[^\s]+\.[a-zA-Z0-9]{3,4})\s*(?<source_src>(?:(source_[^\s.:\/]+:\s+[^\s]+\.[a-zA-Z0-9]{3,4})\s*)+)?(?<html_attr>[\s\S]+)?$/.match(render_markup)
+      markup = /^(?:(?<preset>[^\s.:\/]+)\s+)?"(?<image_src>.+\.[a-zA-Z0-9]{3,4})"\s*(?<source_src>(?:(source_[^\s.:\/]+:\s+[^\s]+\.[a-zA-Z0-9]{3,4})\s*)+)?(?<html_attr>[\s\S]+)?$/.match(render_markup)
       preset = settings['presets'][ markup[:preset] ] || settings['presets']['default']
 
       raise "Picture Tag can't read this tag. Try {% picture [preset] path/to/img.jpg [source_key: path/to/alt-img.jpg] [attr=\"value\"] %}." unless markup
@@ -247,7 +248,7 @@ module Jekyll
       end
 
       # Return path relative to the site root for html
-      Pathname.new(File.join(baseurl, image_dest, image_dir, gen_name)).cleanpath
+      Pathname.new(URI.escape(File.join(baseurl, image_dest, image_dir, gen_name))).cleanpath
     end
   end
 end
