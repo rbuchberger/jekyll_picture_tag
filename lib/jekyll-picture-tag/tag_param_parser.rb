fcc48378799d9ce2d58176aa1e3feec448bd2822
Robert Buchberger <rbuchberger@gmail.com>
2018-10-12 15:21:57 +0200

    Updated parser to avoid instance variables

--- lib/jekyll-picture-tag/tag_param_parser.rb
+++ lib/jekyll-picture-tag/tag_param_parser.rb
@@ -1,34 +1,38 @@
 module PictureTag
-  def parse_tag_params
+  def parse_tag_params(raw_params, context)
     # Raw argument example example:
     # [preset] img.jpg [source_key: alt-img.jpg] [attr=\"value\"]
 
-    params = liquid_lookup.split
+    params = liquid_lookup(raw_params, context).split
+    instructions = {
+      source_images: {}
+    }
 
     # The preset is the first parameter, unless it's a filename.
     # This regex is really easy to fool. Will improve later.
     unless params.first =~ /[^\s.]+.\w+/
-      @instructions[:preset_name] = params.shift
+      instructions[:preset_name] = params.shift
     end
 
     # The next parameter will be the image source.
-    @instructions[:source_image] = params.shift
+    instructions[:source_images].default = params.shift
 
     # Check if the next param is a source key, and if so assign it to the
     # local variable source_key.
     while params.first =~ /(?<source_key>\w+):/
       # If there is a source_key, add it as a hash
       params.shift # throw away the param, we already have the key
-      @instructions[:alt_source_images] << { source_key => params.shift }
+      instructions[:source_images][source_key] = params.shift
     end
 
     # Anything left will be html attributes
-    @instructions[:html_attributes] = params.shift.join ' '
+    instructions[:html_attributes] = params.join ' '
+    instructions
   end
 
-  def liquid_lookup
+  def liquid_lookup(params, context)
     # Render any liquid variables in tag arguments and unescape template code
-    Liquid::Template.parse(@raw_params)
+    Liquid::Template.parse(params)
                     .render(context)
                     .gsub(/\\\{\\\{|\\\{\\%/, '\{\{' => '{{', '\{\%' => '{%')
   end
