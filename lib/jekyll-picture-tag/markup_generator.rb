320a23e4f2aa72b77af58221427d41f619ae4a60
Robert Buchberger <rbuchberger@gmail.com>
2018-10-12 18:15:46 +0200

    Shower Thought: srcset deserves its own class

--- lib/jekyll-picture-tag/markup_generator.rb
+++ lib/jekyll-picture-tag/markup_generator.rb
@@ -7,17 +7,6 @@ module PictureTag
     tag
   end
 
-  def build_ppi_srcset(source_name, attributes)
-    # Returns a srcset
-    srcset_list = ppi_list.collect do |ppi|
-      entry = filename(@instructions[:source_images][source_name],
-                       source_name,
-                       ppi,
-                       attributes[:format])
-      entry << " #{ppi}x"
-    end
-    srcset_list.join ', '
-  end
 
   def build_fallback_image
     img = SingleTag.new 'img'
