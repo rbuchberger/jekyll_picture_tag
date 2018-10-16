a261f6b383bb156e110c324c59664f27ce1b30f3
Robert Buchberger <rbuchberger@gmail.com>
2018-10-16 13:00:45 +0200

    Moved generate image function to GeneratedImage class

--- lib/jekyll-picture-tag.rb
+++ lib/jekyll-picture-tag.rb
@@ -76,8 +76,8 @@ module PictureTag
 
     def render(context)
       @context = context
-      # Gather settings
 
+      # Gather settings
       initialize_instructions
 
       # Prevent Jekyll from erasing our generated files.  This should possibly
@@ -100,12 +100,6 @@ module PictureTag
           source_tags += "#{markdown_escape * 4}<source srcset=\"#{url}#{instance[source][:generated_src]}\"#{media}>\n"
         end
 
-        # Note: we can't indent html output because markdown parsers will turn 4 spaces into code blocks
-        # Note: Added backslash+space escapes to bypass markdown parsing of indented code below -WD
-        picture_tag = "<picture>\n"\
-                      "#{source_tags}"\
-                      "#{markdown_escape * 4}<img src=\"#{url}#{instance['source_default'][:generated_src]}\" #{html_attr_string}>\n"\
-                      "#{markdown_escape * 2}</picture>\n"
       elsif config_settings['markup'] == 'interchange'
 
         interchange_data = []
@@ -131,77 +125,6 @@ module PictureTag
 
       # Return the markup!
       picture_tag.to_s
-    end
-
-    def generate_image(instance, site_source, site_dest, image_source, image_dest, baseurl)
-      begin
-        digest = Digest::MD5.hexdigest(File.read(File.join(site_source, image_source, instance[:src]))).slice!(0..5)
-      rescue Errno::ENOENT
-        warn 'Warning:'.yellow + " source image #{instance[:src]} is missing."
-        return ''
-      end
-
-      image_dir = File.dirname(instance[:src])
-      ext = File.extname(instance[:src])
-      basename = File.basename(instance[:src], ext)
-
-      size = FastImage.size(File.join(site_source, image_source, instance[:src]))
-      orig_width = size[0]
-      orig_height = size[1]
-      orig_ratio = orig_width * 1.0 / orig_height
-
-      gen_width = if instance[:width]
-                    instance[:width].to_f
-                  elsif instance[:height]
-                    orig_ratio * instance[:height].to_f
-                  else
-                    orig_width
-                  end
-      gen_height = if instance[:height]
-                     instance[:height].to_f
-                   elsif instance[:width]
-                     instance[:width].to_f / orig_ratio
-                   else
-                     orig_height
-                   end
-      gen_ratio = gen_width / gen_height
-
-      # Don't allow upscaling. If the image is smaller than the requested dimensions, recalculate.
-      if orig_width < gen_width || orig_height < gen_height
-        undersize = true
-        gen_width = orig_ratio < gen_ratio ? orig_width : orig_height * gen_ratio
-        gen_height = orig_ratio > gen_ratio ? orig_height : orig_width / gen_ratio
-      end
-
-      gen_name = "#{basename}-#{gen_width.round}by#{gen_height.round}-#{digest}#{ext}"
-      gen_dest_dir = File.join(site_dest, image_dest, image_dir)
-      gen_dest_file = File.join(gen_dest_dir, gen_name)
-
-      # Generate resized files
-      unless File.exist?(gen_dest_file)
-
-        warn 'Warning:'.yellow + " #{instance[:src]} is smaller than the requested output file. It will be resized without upscaling." if undersize
-
-        #  If the destination directory doesn't exist, create it
-        FileUtils.mkdir_p(gen_dest_dir) unless File.exist?(gen_dest_dir)
-
-        # Let people know their images are being generated
-        puts "Generating #{gen_name}"
-
-        image = MiniMagick::Image.open(File.join(site_source, image_source, instance[:src]))
-        # Scale and crop
-        image.combine_options do |i|
-          i.resize "#{gen_width}x#{gen_height}^"
-          i.gravity 'center'
-          i.crop "#{gen_width}x#{gen_height}+0+0"
-          i.strip
-        end
-
-        image.write gen_dest_file
-      end
-
-      # Return path relative to the site root for html
-      Pathname.new(File.join(baseurl, image_dest, image_dir, gen_name)).cleanpath
     end
   end
 end
