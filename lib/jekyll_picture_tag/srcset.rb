a48622e96daf01981a7c1feb9caa6c16410f87d9
Robert Buchberger <rbuchberger@gmail.com>
2018-10-26 12:07:55 +0200

    Modularized srcset generation

--- lib/jekyll_picture_tag/srcset.rb
+++ lib/jekyll_picture_tag/srcset.rb
@@ -1,42 +1,32 @@
-# Represents a srcset attribute, which can be applied to either a source tag or
-# an img tag. Generates its own files.
-class SrcSet
+# Basic functionality for a srcset, which also handles file generation.
+
+# Classes including this module must implement the to_a method, which
+# accomplishes the following:
+# - Return an array of srcset entries.
+# - Call generate_file for each entry, giving it the desired width in
+#   pixels.
+module BasicSrcset
   attr_reader :media
 
   def initialize(media:, format:, instructions:)
-    @media = media # Associated Media Query
+    @media = media # Associated Media Query, can be nil
     @format = @instructions.process_format(format) # Output format
     @instructions = instructions
 
     # Image filename, relative to jekyll_picture_tag default dir:
     @image = @instructions.source_images[@media]
-    # Array of sizes to construct images for.
-    @widths = @instructions.widths[@media]
-  end
-
-  def to_a
-    @widths.collect { |w| build_srcset_entry(w) }
   end
 
   def to_s
     to_a.join(', ')
   end
 
+  # Allows us to add a type attribute to whichever element contains this srcset.
   def mime_type
     mime_types[@format]
   end
 
   private
-
-  def build_srcset_entry(width)
-    file = generate_file(width)
-    srcset_entry_text file
-  end
-
-  # Building block for a basic srcset entry. Don't include commas.
-  def srcset_entry_text(file)
-    "#{@instructions.build_url(file.name)} #{file.width}w"
-  end
 
   def generate_file(width)
     GeneratedImage.new(
@@ -48,8 +38,8 @@ class SrcSet
     )
   end
 
-  # Yeah, I know hardcoding these isn't ideal, but I'm not pulling in a new
-  # dependency for 9 lines of easy code.
+  # Hardcoding these isn't ideal, but I'm not pulling in a new dependency for 9
+  # lines of easy code.
   def mime_types
     {
       'gif'  => 'image/gif',
