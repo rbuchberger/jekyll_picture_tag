9962b872ab5325f45636193c66c7ba0e9dd83126
Robert Buchberger <rbuchberger@gmail.com>
2018-11-16 11:53:28 +0100

    auto class updated to use configuration

--- lib/jekyll-picture-tag/output_formats/auto.rb
+++ lib/jekyll-picture-tag/output_formats/auto.rb
@@ -1,20 +1,17 @@
 module PictureTag
   module OutputFormats
-    # Automatically select which output format to return.
+    # This is an odd class, which never returns itself. It selects the most
+    # appropriate format, and returns an instance of that class.
     class Auto
-      def initialize(instructions)
-        formats = instructions.preset['formats'].length
-        sources = instructions.source_images.length
+      def self.new
+        formats = PictureTag.config.preset['formats'].length
+        sources = PictureTag.config.source_images.length
 
-        @element = if formats > 1 || sources > 1
-                     Picture.new(instructions)
-                   else
-                     Img.new(instructions)
-                   end
-      end
-
-      def to_s
-        @element.to_s
+        if formats > 1 || sources > 1
+          Picture.new
+        else
+          Img.new
+        end
       end
     end
   end
