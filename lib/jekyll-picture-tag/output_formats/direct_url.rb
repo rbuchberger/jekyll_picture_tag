66c0eb67acac25a3dfe772f26c8b2ff3994e13f8
Robert Buchberger <rbuchberger@gmail.com>
2019-01-22 09:54:47 +0100

    minor nitpicks to direct_url syntax

--- lib/jekyll-picture-tag/output_formats/direct_url.rb
+++ lib/jekyll-picture-tag/output_formats/direct_url.rb
@@ -1,11 +1,12 @@
 module PictureTag
   module OutputFormats
-    # Represents a bare url you can use in another context, such as a direct link, but keep the resizing functionality
+    # Represents a bare url you can use in another context, such as a direct
+    # link, but keep the resizing functionality
     class DirectUrl
       include PictureTag::OutputFormats::Basics
 
       def to_s
-        build_base_img.attributes.content[:src].first rescue ""
+        build_base_img.src
       end
     end
   end
