3781a8bd5ef13dd5fb5d0edaad6c9f5a7584b764
Robert Buchberger <rbuchberger@gmail.com>
2019-02-11 17:13:42 +0100

    support for temporary src settings

--- lib/jekyll-picture-tag/output_formats/data_attributes.rb
+++ lib/jekyll-picture-tag/output_formats/data_attributes.rb
@@ -7,6 +7,14 @@ module PictureTag
         build_noscript(super)
       end
 
+      def build_base_img
+        img = super
+
+        add_temp_src img
+
+        img
+      end
+
       private
 
       def add_src(element, name)
@@ -38,6 +46,29 @@ module PictureTag
           # Markdown fix requires removal of line breaks:
           oneline: PictureTag.nomarkdown?
         )
+      end
+
+      # Looks at temp_src setting in preset. Adds either a generated image or
+      # the defined generic source image.
+      def add_temp_src(img)
+        return unless (preset_src = PictureTag.preset['temp_src'])
+
+        img.src = if preset_src.is_a? Integer
+                    generated_src preset_src
+                  else
+                    preset_src
+                  end
+      end
+
+      # Takes our fallback image and adds a very low res src
+      def generated_src(width)
+        image = GeneratedImage.new(
+          source_file: PictureTag.source_images[nil],
+          format: PictureTag.fallback_format,
+          width: width
+        )
+
+        PictureTag.build_url image.name
       end
     end
   end
