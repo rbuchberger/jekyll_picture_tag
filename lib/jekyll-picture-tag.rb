e5e7bf02bdd4107e1f72b4481729041d0d9c9f3e
Robert Buchberger <robert@buchberger.cc>
2019-02-22 20:52:15 +0100

    improve warning message

--- lib/jekyll-picture-tag.rb
+++ lib/jekyll-picture-tag.rb
@@ -1,17 +1,20 @@
 puts '-'*80
 
 puts <<-HEREDOC
-
-Message from Jekyll Picture Tag:
-
+\033[31;1;4m
+Important message from Jekyll Picture Tag:
+\033[0m
 Good news! We're back up on rubygems. 
-Bad news. You need to update your gemfile. Sorry about that. We had to rename
-the whole gem. For an explanation, see issue #120:
-https://github.com/robwierzbowski/jekyll-picture-tag/issues/120
+Bad news. You need to update your Gemfile. Remove the following line:
 
-In your gemfile, remove the 'jekyll-picture-tag' line, replace it with:
+gem 'jekyll-picture-tag', git: 'https://github.com/robwierzbowski/jekyll-picture-tag/'
+
+and replace it with:
 
 gem 'jekyll_picture_tag', '~> 1.2'
+
+Sorry about that. For an explanation, see issue #120:
+https://github.com/robwierzbowski/jekyll-picture-tag/issues/120
 
 HEREDOC
 
