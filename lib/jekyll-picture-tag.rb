768991be43d45b056bbd79f4e71bfb16ba480313
Robert Buchberger <robert@buchberger.cc>
2019-06-26 11:55:47 +0200

    update error message

--- lib/jekyll-picture-tag.rb
+++ lib/jekyll-picture-tag.rb
@@ -1,23 +1,23 @@
-puts '-'*80
+puts '-' * 80
 
-puts <<-HEREDOC
-\033[31;1;4m
-Important message from Jekyll Picture Tag:
-\033[0m
-Good news! We're back up on rubygems. 
-Bad news. You need to update your Gemfile. Remove the following line:
+puts <<~HEREDOC
+  \033[31;1;4m
+  Important message from Jekyll Picture Tag:
+  \033[0m
+  Good news! We're back up on rubygems.
+  Bad news. You need to update your Gemfile. Remove the following line:
 
-gem 'jekyll-picture-tag', git: 'https://github.com/robwierzbowski/jekyll-picture-tag/'
+  gem 'jekyll-picture-tag', git: 'https://github.com/robwierzbowski/jekyll-picture-tag/'
 
-and replace it with:
+  and replace it with:
 
-gem 'jekyll_picture_tag', '~> 1.2'
+  gem 'jekyll_picture_tag', '~> 1.3'
 
-Sorry about that. For an explanation, see issue #120:
-https://github.com/robwierzbowski/jekyll-picture-tag/issues/120
+  Sorry about that. For an explanation, see issue #120:
+  https://github.com/rbuchberger/jekyll-picture-tag/issues/120
 
 HEREDOC
 
-puts '-'*80
+puts '-' * 80
 
 require_relative 'jekyll_picture_tag'
