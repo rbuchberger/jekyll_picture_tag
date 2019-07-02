cee3404848a6572ed15daaf3b55a697302511839
Robert Buchberger <robert@buchberger.cc>
2019-07-02 20:49:54 +0200

    Refactor utils module

--- lib/jekyll_picture_tag/utils.rb
+++ lib/jekyll_picture_tag/utils.rb
@@ -2,70 +2,66 @@ module PictureTag
   # This is a little module to hold logic that doesn't fit other places. If it
   # starts getting big, refactor.
   module Utils
-    # Configure Jekyll to keep our generated files
-    def self.keep_files
-      dest_dir = PictureTag.config['picture']['output']
+    class << self
+      # Configure Jekyll to keep our generated files
+      def keep_files
+        dest_dir = PictureTag.config['picture']['output']
 
-      # Chop a slash off the end, if it's there. Doesn't work otherwise.
-      dest_dir = dest_dir[0..-2] if dest_dir =~ %r{/\z}
+        # Chop a slash off the end, if it's there. Doesn't work otherwise.
+        dest_dir = dest_dir[0..-2] if dest_dir =~ %r{/\z}
 
-      return if PictureTag.site.config['keep_files'].include?(dest_dir)
+        return if PictureTag.site.config['keep_files'].include?(dest_dir)
 
-      PictureTag.site.config['keep_files'] << dest_dir
-    end
-
-    # Print a warning to the console
-    def self.warning(message)
-      return if PictureTag.config['picture']['suppress_warnings']
-
-      warn 'Jekyll Picture Tag Warning: '.yellow + message
-    end
-
-    # Parse a liquid template; allows liquid variables to be included as tag
-    # params.
-    def self.liquid_lookup(params)
-      Liquid::Template.parse(params).render(PictureTag.context)
-
-      # This gsub allows people to include template code for javascript
-      # libraries such as handlebar.js. It adds complication and I'm not sure
-      # it has much value now, so I'm commenting it out. If someone has a use
-      # case for it we can add it back in.
-      # .gsub(/\\\{\\\{|\\\{\\%/, '\{\{' => '{{', '\{\%' => '{%')
-    end
-
-    # Allows us to use 'original' as a format name.
-    def self.process_format(format, media)
-      if format.casecmp('original').zero?
-        PictureTag.source_images[media].ext
-      else
-        format.downcase
+        PictureTag.site.config['keep_files'] << dest_dir
       end
-    end
 
-    # Used for auto markup configuration and such
-    def self.count_srcsets
-      formats = PictureTag.preset['formats'].length
-      source_images = PictureTag.source_images.length
+      # Print a warning to the console
+      def warning(message)
+        return if PictureTag.config['picture']['suppress_warnings']
 
-      formats * source_images
-    end
+        warn 'Jekyll Picture Tag Warning: '.yellow + message
+      end
 
-    # Returns whether or not the current page is a markdown file.
-    def self.markdown_page?
-      page_name = PictureTag.page['name']
-      page_ext = PictureTag.page['ext']
-      ext = page_ext ? page_ext : File.extname(page_name)
+      # Parse a liquid template; allows liquid variables to be included as tag
+      # params.
+      def liquid_lookup(params)
+        Liquid::Template.parse(params).render(PictureTag.context)
+      end
 
-      ext.casecmp('.md').zero? || ext.casecmp('.markdown').zero?
-    end
+      # Allows us to use 'original' as a format name.
+      def process_format(format, media)
+        if format.casecmp('original').zero?
+          PictureTag.source_images[media].ext
+        else
+          format.downcase
+        end
+      end
 
-    # Returns the widest source image
-    def self.biggest_source
-      PictureTag.source_images.values.max_by(&:width)
-    end
+      # Used for auto markup configuration and such
+      def count_srcsets
+        formats = PictureTag.preset['formats'].length
+        source_images = PictureTag.source_images.length
 
-    def self.titleize(input)
-      input.split('_').map(&:capitalize).join
+        formats * source_images
+      end
+
+      # Returns whether or not the current page is a markdown file.
+      def markdown_page?
+        page_name = PictureTag.page['name']
+        page_ext = PictureTag.page['ext']
+        ext = page_ext || File.extname(page_name)
+
+        ext.casecmp('.md').zero? || ext.casecmp('.markdown').zero?
+      end
+
+      # Returns the widest source image
+      def biggest_source
+        PictureTag.source_images.values.max_by(&:width)
+      end
+
+      def titleize(input)
+        input.split('_').map(&:capitalize).join
+      end
     end
   end
 end
