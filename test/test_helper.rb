6ebc9adff8606b0ad58442449e1ce4eb965ef96e
Robert Buchberger <robert@buchberger.cc>
2019-07-03 18:07:01 +0200

    create test helper

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,3 +1,10 @@
 require 'minitest/autorun'
 require 'mocha/minitest'
 require 'jekyll_picture_tag'
+
+module TestHelper
+  LiquidTemplate = Struct.new(:render)
+  ImageStruct = Struct.new(:width)
+  SiteStub = Struct.new(:config)
+  ConfigStub = Struct.new(:source_dir)
+end
