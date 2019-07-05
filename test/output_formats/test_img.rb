76bd3e71efb320e88ce866a633b1bbb3f9888abe
Robert Buchberger <robert@buchberger.cc>
2019-07-05 18:55:42 +0200

    Change testing strategy
    Most of those tests I wrote over the last 10ish commits suck. I was
    testing methods, not implementation. The big red flag I should have seen
    was stubbing methods on the tested class!
    
    I'm not going to test private methods, or methods in general. I'm going
    to test each class as a whole, against all the various inputs it will
    receive. The 'unit' in unit test isn't a method, it's a class.
    
    This commit eliminates tests of classes that are never instantiated, and
    tests the img output format. All other PictureTag classes are stubbed,
    but ObjectiveElements is left untouched since it is so integral to its
    output. I could remove this dependency by stubbing the OE API, but it's
    a tiny and simple library and simply verifying an output string is much
    easier.

--- test/output_formats/test_img.rb
+++ test/output_formats/test_img.rb
@@ -5,31 +5,122 @@ class TestOutputFormatImg < Minitest::Test
   include TestHelper
 
   def setup
+    GeneratedImage
+      .stubs(:new)
+      .returns(GeneratedImageStub.new(name: 'generated.img', width: 999))
+    PictureTag::Srcsets::Basic
+      .stubs(:new)
+      .returns(SrcsetWidthStub.new(nil, 'srcset', nil))
+    PictureTag.stubs(fallback_format: 'fallback format',
+                     fallback_width: 100,
+                     preset: { 'widths' => [100, 200, 300] },
+                     source_images: { nil => 'img.jpg' },
+                     formats: ['original'],
+                     build_url: 'good_url',
+                     html_attributes: {},
+                     nomarkdown: false)
+
     @tested = OutputFormats::Img.new
   end
 
-  # srcset
-  def test_srcset
-    PictureTag.expects(:formats).returns(['some format'])
-    @tested.expects(:build_srcset).with(nil, 'some format')
-    @tested.srcset
+  def pixel_ratio_stub
+    SrcsetWidthStub.new(nil, 'pixel srcset', nil)
   end
 
-  # base_markup
-  def test_base_markup
-    dummy_img = Object.new
-    dummy_srcset = Object.new
-    dummy_attributes = Object.new
-
-    @tested.expects(:build_base_img).returns(dummy_img)
-    @tested.stubs(:srcset).returns(dummy_srcset)
-    @tested.expects(:add_srcset).with(dummy_img, dummy_srcset)
-    @tested.expects(:add_sizes).with(dummy_img, dummy_srcset)
-
-    dummy_img.expects(:attributes).returns(dummy_attributes)
-    dummy_attributes.expects(:<<).with('attributes')
-    PictureTag.stubs(:html_attributes).returns('parent' => 'attributes')
-
-    @tested.base_markup
+  def width_stub
+    SrcsetWidthStub.new(nil, 'width srcset', nil)
   end
+
+  # Test cases:
+  # basic img
+  def test_img
+    correct = <<~HEREDOC
+      <img src="good_url" srcset="srcset">
+    HEREDOC
+
+    assert_equal correct, @tested.to_s
+  end
+
+  # img with pixel ratio srcset
+  def test_img_pixel_ratio
+    PictureTag.stubs(:preset)
+              .returns('pixel_ratios' => [1, 2], 'base_width' => 100)
+
+    Srcsets::PixelRatio.expects(:new).returns(pixel_ratio_stub)
+    correct = <<~HEREDOC
+      <img src="good_url" srcset="pixel srcset">
+    HEREDOC
+    assert_equal correct, @tested.to_s
+  end
+
+  # img with width srcset
+  def test_width_srcset
+    correct = <<~HEREDOC
+      <img src="good_url" srcset="width srcset">
+    HEREDOC
+
+    Srcsets::Width.expects(:new).returns(width_stub)
+    assert_equal correct, @tested.to_s
+  end
+
+  # img with srcset and sizes
+  def test_srcset_and_sizes
+    Srcsets::Width.stubs(:new).returns(
+      SrcsetWidthStub.new('correct sizes', 'width srcset')
+    )
+
+    correct = <<~HEREDOC
+      <img src="good_url" srcset="width srcset" sizes="correct sizes">
+    HEREDOC
+
+    assert_equal correct, @tested.to_s
+  end
+
+  # img with alt
+  def test_alt
+    PictureTag.stubs(:html_attributes).returns('alt' => 'alt text')
+
+    correct = <<~HEREDOC
+      <img src="good_url" alt="alt text" srcset="srcset">
+    HEREDOC
+
+    assert_equal correct, @tested.to_s
+  end
+
+  # img with parent attributes
+  def test_parent
+    PictureTag.stubs(:html_attributes).returns('parent' => 'class="parent"')
+
+    correct = <<~HEREDOC
+      <img src="good_url" srcset="srcset" class="parent">
+    HEREDOC
+
+    assert_equal correct, @tested.to_s
+  end
+  # img with img attributes
+
+  def test_img_attrs
+    PictureTag.stubs(:html_attributes).returns('img' => 'class="img"')
+
+    correct = <<~HEREDOC
+      <img class="img" src="good_url" srcset="srcset">
+    HEREDOC
+
+    assert_equal correct, @tested.to_s
+  end
+
+  # img with both img and parent attrs
+  def test_both_attrs
+    PictureTag.stubs(:html_attributes)
+              .returns('img' => 'class="img"', 'parent' => 'class="parent"')
+
+    correct = <<~HEREDOC
+      <img class="img parent" src="good_url" srcset="srcset">
+    HEREDOC
+
+    assert_equal correct, @tested.to_s
+  end
+
+  # anchor tag wrapper
+  # nomarkdown wrapper
 end
