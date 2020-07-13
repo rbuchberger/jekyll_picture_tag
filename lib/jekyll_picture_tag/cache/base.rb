255b57ec45fcec054fda2584da6353f9e705e818
Robert Buchberger <robert@buchberger.cc>
2020-07-13 16:51:30 +0200

    Teach cache to handle images in subdirectories

--- lib/jekyll_picture_tag/cache/base.rb
+++ lib/jekyll_picture_tag/cache/base.rb
@@ -20,9 +20,9 @@ module PictureTag
 
       # Call after updating data.
       def write
-        FileUtils.mkdir_p directory
+        FileUtils.mkdir_p(File.join(base_directory, sub_directory))
 
-        File.open(filename, 'w') do |f|
+        File.open(filename, 'w+') do |f|
           f.write JSON.generate(data)
         end
       end
@@ -37,13 +37,28 @@ module PictureTag
                   end
       end
 
-      def directory
+      # /home/dave/my_blog/.jekyll-cache/jpt/(cache_dir)/assets/myimage.jpg.json
+      # ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
+      def base_directory
         File.join(PictureTag.site.cache_dir, 'jpt', cache_dir)
       end
 
+      # /home/dave/my_blog/.jekyll-cache/jpt/(cache_dir)/assets/myimage.jpg.json
+      #                                                 ^^^^^^^^
+      def sub_directory
+        File.dirname(@base_name)
+      end
+
+      # /home/dave/my_blog/.jekyll-cache/jpt/(cache_dir)/assets/myimage.jpg.json
+      # ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
+      def directory
+        File.join(base_directory, sub_directory)
+      end
+
       # /home/dave/my_blog/.jekyll-cache/jpt/somefolder/myimage.jpg.json
+      # ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
       def filename
-        File.join(directory, @base_name + '.json')
+        File.join(base_directory, @base_name + '.json')
       end
     end
   end
