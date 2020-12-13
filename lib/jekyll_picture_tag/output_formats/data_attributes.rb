0a1d10a51e5156eda25d166f24fd6c9ab1a4957b
stuntbox <hello@stuntbox.com>
2020-12-12 21:53:57 -0500

    First proof of concept hack

--- lib/jekyll_picture_tag/output_formats/data_attributes.rb
+++ lib/jekyll_picture_tag/output_formats/data_attributes.rb
@@ -9,8 +9,9 @@ module PictureTag
 
       private
 
+
       def add_src(element, uri)
-        element.attributes << { 'data-src' => uri }
+        element.attributes << { 'src="data:image/svg+xml,%3Csvg xmlns=\'http://www.w3.org/2000/svg\' viewBox=\'0 0 3 2\'%3E%3C/svg%3E" data-src' => uri }
       end
 
       def add_srcset(element, srcset)
