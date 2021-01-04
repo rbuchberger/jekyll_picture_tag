d5ca62411f0ca952a6da5d96ff89743efef62e30
Robert Buchberger <robert@buchberger.cc>
2021-01-04 17:46:57 +0100

    Remove redundant comments from img_uri unit tests

--- test/unit/test_img_uri.rb
+++ test/unit/test_img_uri.rb
@@ -3,7 +3,6 @@ class ImgUriTest < Minitest::Test
   include PictureTag
   include TestHelper
 
-  # setup
   def setup
     @pconfig = {
       'source' => 'source-dir',
@@ -25,19 +24,16 @@ class ImgUriTest < Minitest::Test
     ImgURI.new(param, source_image: source_image).to_s
   end
 
-  # relative url
   def test_relative
     @pconfig['relative_url'] = true
 
     assert_equal '/output-dir/img.jpg', tested
   end
 
-  # absolute url
   def test_absolute
     assert_equal 'example.com/output-dir/img.jpg', tested
   end
 
-  # cdn url
   def test_cdn
     PictureTag.stubs(:cdn?).returns true
     @pconfig['cdn_url'] = 'some-cdn.net'
@@ -45,7 +41,6 @@ class ImgUriTest < Minitest::Test
     assert_equal 'some-cdn.net/output-dir/img.jpg', tested
   end
 
-  # with baseurl
   def test_baseurl
     @config['baseurl'] = 'some-baseurl'
 
@@ -66,13 +61,11 @@ class ImgUriTest < Minitest::Test
     assert_equal 'example.com/some-baseurl/output-dir/img.jpg', tested
   end
 
-  # source image
   def test_source_image
     assert_equal 'example.com/source-dir/img.jpg',
                  tested('img.jpg', source_image: true)
   end
 
-  # url encoding
   def test_url_encoding
     param = 'white space.jpg'
     assert_equal 'example.com/output-dir/white%20space.jpg', tested(param)
