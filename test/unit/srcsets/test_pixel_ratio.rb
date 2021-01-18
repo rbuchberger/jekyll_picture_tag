a94a0e27bf498e346fce1cbf8a4b523ead7e9223
Robert Buchberger <robert@buchberger.cc>
2021-01-18 18:10:54 +0100

    Refactor Srcset unit tests

--- test/unit/srcsets/test_pixel_ratio.rb
+++ test/unit/srcsets/test_pixel_ratio.rb
@@ -1,17 +1,17 @@
 require_relative 'srcsets_test_helper'
 
 class TestSrcsetPixelRatio < Minitest::Test
-  include TestHelperSrcset
-  def setup
-    build_source_stub
-    build_genstubs
+  include SrcsetTestHelper
 
-    PictureTag.stubs(preset: {
-                       'pixel_ratios' => [1, 1.5, 2, 3],
-                       'base_width' => 100
-                     }, crop: nil, gravity: 'center')
+  def tested
+    Srcsets::PixelRatio.new(source_image, 'original')
+  end
 
-    @tested = Srcsets::PixelRatio.new(@source_image, 'original')
+  def config
+    { gravity: 'center', crop: nil, preset: {
+      'pixel_ratios' => [1, 1.5, 2, 3],
+      'base_width' => 100
+    } }
   end
 
   def test_basic
@@ -21,6 +21,6 @@ class TestSrcsetPixelRatio < Minitest::Test
       '/img-200-aaaaa.jpg 2.0x, ' \
       '/img-300-aaaaa.jpg 3.0x'
 
-    assert_equal correct, @tested.to_s
+    assert_equal correct, tested.to_s
   end
 end
--- test/unit/srcsets/test_width.rb
+++ test/unit/srcsets/test_width.rb
@@ -1,15 +1,14 @@
 require_relative 'srcsets_test_helper'
 
 class TestSrcsetWidth < Minitest::Test
-  include TestHelperSrcset
+  include SrcsetTestHelper
 
-  def setup
-    build_source_stub
-    build_genstubs
+  def tested
+    Srcsets::Width.new(source_image, 'original')
+  end
 
-    PictureTag.stubs(widths: [100, 200, 300], crop: nil, gravity: 'center')
-
-    @tested = Srcsets::Width.new(@source_image, 'original')
+  def config
+    { widths: [100, 200, 300], crop: nil, gravity: 'center' }
   end
 
   def stub_picturetag_sizes
@@ -22,73 +21,63 @@ class TestSrcsetWidth < Minitest::Test
                      })
   end
 
-  # basic srcset
   def test_basic
     correct =
       '/img-100-aaaaa.jpg 100w, ' \
       '/img-200-aaaaa.jpg 200w, ' \
       '/img-300-aaaaa.jpg 300w'
 
-    assert_equal correct, @tested.to_s
+    assert_equal correct, tested.to_s
   end
 
   def test_image_generation
     PictureTag.stubs(widths: [600])
+    stub_generated(600, stub = gstub(600))
 
-    stub = gstub(600)
-
-    GeneratedImage.expects(:new)
-                  .with(source_file: @source_image, width: 600,
-                        format: 'original', crop: nil, gravity: 'center')
-                  .returns(stub)
     stub.expects(:generate)
-    @tested.to_s
+
+    tested.to_s
   end
 
-  # srcset with sizes
   def test_sizes
     stub_picturetag_sizes
 
     correct = '(mobile query) mobile size, regular size'
 
-    assert_equal correct, @tested.sizes
+    assert_equal correct, tested.sizes
   end
 
-  # srcset mime type
   def test_mime_type
-    assert_equal 'image/jpeg', @tested.mime_type
+    assert_equal 'image/jpeg', tested.mime_type
   end
 
-  # media query name
   def test_media
-    assert_equal 'mobile', @tested.media
+    assert_equal 'mobile', tested.media
   end
 
-  # media attribute
   def test_media_attribute
     stub_picturetag_sizes
 
-    assert_equal '(mobile query)', @tested.media_attribute
+    assert_equal '(mobile query)', tested.media_attribute
   end
 
-  # small source
   def test_small_source
-    @source_image.width = 150
+    source_image.width = 150
 
     correct =
       '/img-100-aaaaa.jpg 100w, ' \
       '/img-150-aaaaa.jpg 150w'
 
     Utils.expects(:warning)
-    assert_equal correct, @tested.to_s
+    assert_equal correct, tested.to_s
   end
 
   # Make sure we don't check image width unnecessarily.
   def test_unneeded_widths_check
     GeneratedImage.unstub(:new)
-    GeneratedImage.stubs(:new).returns(gstub(600, exists: true))
-    @source_image.expects(:width).never
+    GeneratedImage.stubs(new: gstub(600, exists: true))
+    source_image.expects(:width).never
 
-    @tested.to_s
+    tested.to_s
   end
 end
