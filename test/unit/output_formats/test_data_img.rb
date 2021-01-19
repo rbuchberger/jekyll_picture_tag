f6d8de59dfa2acdd25f75248f987f7eaef48d3ee
Robert Buchberger <robert@buchberger.cc>
2021-01-19 16:43:32 +0100

    Refactor Img & DataImg output format tests

--- test/unit/output_formats/test_data_img.rb
+++ test/unit/output_formats/test_data_img.rb
@@ -1,27 +1,30 @@
-require_relative './output_format_test_helper'
+require_relative 'output_format_test_helper'
 
 class TestDataImg < Minitest::Test
-  include PictureTag
-  include TestHelper
   include OutputFormatTestHelper
+
+  # Lifecycle
 
   def setup
     base_stubs
+  end
 
-    @tested = OutputFormats::DataImg.new
+  # Helpers
+
+  def tested
+    @tested ||= OutputFormats::DataImg.new
   end
 
   # Test cases:
-  # basic img
+
   def test_img
     correct = <<~HEREDOC
       <img data-src="good_url" data-srcset="ss">
     HEREDOC
 
-    assert_equal correct, @tested.to_s
+    assert_equal correct, tested.to_s
   end
 
-  # img with srcset and sizes
   def test_srcset_and_sizes
     stub_sizes_srcset
 
@@ -29,24 +32,21 @@ class TestDataImg < Minitest::Test
       <img data-src="good_url" data-srcset="width srcset" data-sizes="correct sizes">
     HEREDOC
 
-    assert_equal correct, @tested.to_s
+    assert_equal correct, tested.to_s
   end
 
   def test_noscript
-    PictureTag.stubs(preset: { 'noscript' => true }, nomarkdown: false)
-
-    img_stub = Object.new
-    img_stub.stubs(:build_base_img).returns('<img src="good-url">')
-    OutputFormats::Img.stubs(:new).returns(img_stub)
+    PictureTag.preset['noscript'] = true
+    PictureTag.stubs(nomarkdown: false)
 
     correct = <<~HEREDOC
       <img data-src="good_url" data-srcset="ss">
       <noscript>
-        <img src="good-url">
+        <img src="good_url">
       </noscript>
 
     HEREDOC
 
-    assert_equal correct, @tested.to_s
+    assert_equal correct, tested.to_s
   end
 end
--- test/unit/output_formats/test_img.rb
+++ test/unit/output_formats/test_img.rb
@@ -1,38 +1,40 @@
-require_relative './output_format_test_helper'
+require_relative 'output_format_test_helper'
 
 class TestImg < Minitest::Test
-  include PictureTag
-  include TestHelper
   include OutputFormatTestHelper
+
+  # Lifecycle
 
   def setup
     base_stubs
-
-    @tested = OutputFormats::Img.new
   end
 
-  # Test cases:
-  # basic img
+  # Helpers
+
+  def tested
+    @tested ||= OutputFormats::Img.new
+  end
+
+  # Test cases
+
   def test_img
     correct = <<~HEREDOC
       <img src="good_url" srcset="ss">
     HEREDOC
 
-    assert_equal correct, @tested.to_s
+    assert_equal correct, tested.to_s
   end
 
-  # img with pixel ratio srcset
   def test_img_pixel_ratio
-    pixel_ratio_setup
+    PictureTag.preset.merge! pixel_ratio_preset
     stub_pixel_srcset
 
     correct = <<~HEREDOC
       <img src="good_url" srcset="pixel srcset">
     HEREDOC
-    assert_equal correct, @tested.to_s
+    assert_equal correct, tested.to_s
   end
 
-  # img with width srcset
   def test_width_srcset
     stub_width_srcset
 
@@ -40,10 +42,9 @@ class TestImg < Minitest::Test
       <img src="good_url" srcset="width srcset">
     HEREDOC
 
-    assert_equal correct, @tested.to_s
+    assert_equal correct, tested.to_s
   end
 
-  # img with srcset and sizes
   def test_srcset_and_sizes
     stub_sizes_srcset
 
@@ -51,58 +52,52 @@ class TestImg < Minitest::Test
       <img src="good_url" srcset="width srcset" sizes="correct sizes">
     HEREDOC
 
-    assert_equal correct, @tested.to_s
+    assert_equal correct, tested.to_s
   end
 
-  # img with alt
   def test_alt
-    PictureTag.stubs(:html_attributes).returns('alt' => 'alt text')
+    PictureTag.html_attributes.merge!({ 'alt' => 'alt text' })
 
     correct = <<~HEREDOC
       <img src="good_url" alt="alt text" srcset="ss">
     HEREDOC
 
-    assert_equal correct, @tested.to_s
+    assert_equal correct, tested.to_s
   end
 
-  # img with parent attributes
   def test_parent
-    PictureTag.stubs(:html_attributes).returns('parent' => 'class="parent"')
+    PictureTag.html_attributes.merge!({ 'parent' => 'class="parent"' })
 
     correct = <<~HEREDOC
       <img src="good_url" srcset="ss" class="parent">
     HEREDOC
 
-    assert_equal correct, @tested.to_s
+    assert_equal correct, tested.to_s
   end
 
-  # img with img attributes
   def test_img_attrs
-    PictureTag.stubs(:html_attributes).returns('img' => 'class="img"')
+    PictureTag.html_attributes.merge!({ 'img' => 'class="img"' })
 
     correct = <<~HEREDOC
       <img class="img" src="good_url" srcset="ss">
     HEREDOC
 
-    assert_equal correct, @tested.to_s
+    assert_equal correct, tested.to_s
   end
 
-  # img with both img and parent attrs
   def test_both_attrs
-    PictureTag.stubs(:html_attributes)
-              .returns('img' => 'class="img"', 'parent' => 'class="parent"')
+    PictureTag.html_attributes.merge!({ 'img' => 'class="img"',
+                                        'parent' => 'class="parent"' })
 
     correct = <<~HEREDOC
       <img class="img parent" src="good_url" srcset="ss">
     HEREDOC
 
-    assert_equal correct, @tested.to_s
+    assert_equal correct, tested.to_s
   end
 
-  # anchor tag wrapper
   def test_anchor_tag
-    PictureTag.stubs(:html_attributes)
-              .returns('link' => 'some link')
+    PictureTag.html_attributes['link'] = 'some link'
 
     correct = <<~HEREDOC
       <a href="some link">
@@ -110,13 +105,12 @@ class TestImg < Minitest::Test
       </a>
     HEREDOC
 
-    assert_equal correct, @tested.to_s
+    assert_equal correct, tested.to_s
   end
 
-  # anchor with attributes
   def test_anchor_with_attrs
-    PictureTag.stubs(:html_attributes)
-              .returns('link' => 'some link', 'a' => 'class="anchor"')
+    PictureTag.html_attributes.merge!({ 'link' => 'some link',
+                                        'a' => 'class="anchor"' })
 
     correct = <<~HEREDOC
       <a class="anchor" href="some link">
@@ -124,19 +118,18 @@ class TestImg < Minitest::Test
       </a>
     HEREDOC
 
-    assert_equal correct, @tested.to_s
+    assert_equal correct, tested.to_s
   end
 
-  # setting is true, with link. Should get oneline with wrapper
   def test_nomarkdown_wrapper
-    PictureTag.stubs(:nomarkdown? => true,
-                     'html_attributes' => { 'link' => 'some link' })
+    PictureTag.stubs(nomarkdown?: true)
+    PictureTag.html_attributes['link'] = 'some link'
 
     correct = <<~HEREDOC
       {::nomarkdown}<a href="some link"><img src="good_url" srcset="ss"></a>{:/nomarkdown}
     HEREDOC
     correct.delete!("\n")
 
-    assert_equal correct, @tested.to_s
+    assert_equal correct, tested.to_s
   end
 end
