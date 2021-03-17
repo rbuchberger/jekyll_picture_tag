fc33e2986eae6cb6f4c687c3c40aea1b0a2e9400
Robert Buchberger <robert@buchberger.cc>
2021-03-17 12:20:31 +0100

    Create stock presets and media queries
    All built-in presets and media queries will have a jpt- prefix.
    
    These are meant to be examples and starting points for further
    customization.

--- lib/jekyll_picture_tag/defaults/presets.rb
+++ lib/jekyll_picture_tag/defaults/presets.rb
@@ -15,4 +15,35 @@ module PictureTag
                        'avif' => { 'compression' => 'av1', 'speed' => 8 }
                      } }.freeze
 
+  STOCK_PRESETS = {
+    'jpt-webp' => { 'formats' => %w[webp original] },
+
+    'jpt-avif' => { 'formats' => %w[avif webp original] },
+
+    'jpt-lazy' => { 'markup' => 'data_auto',
+                    'noscript' => true,
+                    'attributes' => { 'parent' => 'class="lazy"' } },
+
+    'jpt-loaded' => { 'formats' => %w[avif jp2 webp original],
+                      'dimension_attributes' => true },
+
+    'jpt-direct' => { 'markup' => 'direct_url' },
+
+    'jpt-thumbnail' => { 'base_width' => 250,
+                         'pixel_ratios' => [1, 1.5, 2],
+                         'formats' => %w[webp original] },
+
+    'jpt-avatar' => { 'base_width' => 100,
+                      'pixel_ratios' => [1, 1.5, 2],
+                      'formats' => %w[webp original],
+                      'crop' => '1:1' }
+  }.freeze
+
+  STOCK_MEDIA_QUERIES = {
+    'jpt-mobile' => 'max-width: 480px',
+    'jpt-tablet' => 'max-width: 768',
+    'jpt-laptop' => 'max-width: 1024px',
+    'jpt-desktop' => 'max-width: 1200',
+    'jpt-wide' => 'min-width: 1201'
+  }.freeze
 end
