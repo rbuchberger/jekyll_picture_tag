d6928ec7163a4d9abe2d08807dba36d1f5bb19a8
Robert Buchberger <robert@buchberger.cc>
2021-04-06 12:35:34 +0200

    Improve imagemagick format support detection
    `convert -version` output doesn't include all supported formats;
    `convert -list format` does. Use that instead.
    
    Downside is that this is a decent amount of text; parsing it is a
    performance hit. In the future we can cache it per site build, rather
    than per tag instance.

--- lib/jekyll_picture_tag/parsers/image_backend.rb
+++ lib/jekyll_picture_tag/parsers/image_backend.rb
@@ -15,18 +15,18 @@ module PictureTag
       # Returns array of formats that vips can save to
       def vips_formats
         @vips_formats ||= `vips -l filesave`
-                          .scan(/\.[a-z]{1,5}/)
+                          .scan(/\.[a-z\d]{1,6}/)
                           .uniq
                           .map { |format| format.strip.delete_prefix('.') }
       end
 
-      # Returns an array of formats that imagemagick can handle.
+      # Returns an array of formats that imagemagick can read & write
+      # TODO: Cache this. Currently It costs about .1s per liquid tag.
       def magick_formats
-        @magick_formats ||= `convert -version`
+        @magick_formats ||= `convert -list format`
                             .split("\n")
-                            .last
-                            .delete_prefix('Delegates (built-in):')
-                            .split
+                            .select { |i| i.match? magick_regex }
+                            .map { |i| i.split[1].downcase }
       end
 
       # Returns an array of all known names of a format, for the purposes of
@@ -37,6 +37,20 @@ module PictureTag
       end
 
       private
+
+      # Matches these lines:
+      #    FLV  MPEG      rw+   Flash Vid(...)
+      #   JPEG* JPEG      rw-   Joint Pho(...)
+      #
+      # But not these lines:
+      # Format  Module    Mode  Descripti(...)
+      #   -------------------------------(...)
+      #   JSON  JSON      -w+   The image(...)
+      #    K25  DNG       r--   Kodak Dig(...)
+      def magick_regex
+        # /^\s*([A-Z\d\-]+\*?\s+){2}rw[+\-]/
+        /^\s*[A-Z\d\-]+\*?\s+[A-Z\d\-]+\s+rw[+\-]/
+      end
 
       def error_string(format)
         <<~HEREDOC
