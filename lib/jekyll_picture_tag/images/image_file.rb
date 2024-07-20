3e9bfcbe54df19bbbb873f34102dd207429ff2f7
Daiki Ichiyama <dichiban@localhost.localdomain>
2024-07-20 15:57:37 +1000

    Fix: Ignore unsupported quality parameter for PPM files

--- lib/jekyll_picture_tag/images/image_file.rb
+++ lib/jekyll_picture_tag/images/image_file.rb
@@ -57,9 +57,9 @@ module PictureTag
 
       opts[:strip] = PictureTag.preset['strip_metadata']
 
-      # gifs don't accept a quality setting, and PNGs don't on older versions of
+      # gifs don't accept a quality setting, and PNGs and PPMs don't on older versions of
       # vips. Since it's not remarkably useful anyway, we'll ignore them.
-      opts[quality_key] = base.quality unless %w[gif png].include? base.format
+      opts[quality_key] = base.quality unless %w[gif png ppm].include? base.format
 
       opts.transform_keys(&:to_sym)
     end
